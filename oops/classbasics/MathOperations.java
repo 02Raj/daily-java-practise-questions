cd9396dd87592d1d0d47ec43e7f9b3ccefcfd19c
Divyansh Raj <divyanshraj02@gmail.com>
2025-05-12 21:38:01 +0530

    code check in

--- oops/classbasics/MathOperations.java
+++ oops/classbasics/MathOperations.java
@@ -17,9 +17,9 @@ public class MathOperations {
 
     public static void main(String[] args) {
         MathOperations math = new MathOperations();
-        System.out.println(STR."Sum of 2 and 3: \{math.add(2, 3)}");
-        System.out.println(STR."Sum of 2, 3, and 4: \{math.add(2, 3, 4)}");
-        System.out.println(STR."Sum of 2.5 and 3.5: \{math.add(2.5, 3.5)}");
+//        System.out.println(STR."Sum of 2 and 3: \{math.add(2, 3)}");
+//        System.out.println(STR."Sum of 2, 3, and 4: \{math.add(2, 3, 4)}");
+//        System.out.println(STR."Sum of 2.5 and 3.5: \{math.add(2.5, 3.5)}");
     }
 
 }
