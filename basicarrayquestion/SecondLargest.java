1337e7767d93df8639496dc4e6526e2fcf54d0c0
Divyansh <divyansh.raj@tanglesolutions.com>
2025-10-28 16:18:59 +0530

    secondLargest

---
+++ basicarrayquestion/SecondLargest.java
@@ -0,0 +1,24 @@
+package basicarrayquestion;
+
+public class SecondLargest {
+
+    public static int secondLargest(int[] arr){
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int num : arr){
+          if(num > first){
+              second = first;
+              first = num;
+          } else if (num > second && num < first) {
+              second = num;
+          }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 20, 20, 15};
+        int result = secondLargest(arr);
+        System.out.println("result " + result);
+    }
+}
