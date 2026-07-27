632d99597c4b91da41bd97c454f22a8b2d913eba
SLANTCO\divya <divyansh@eatonprojects.com>
2026-07-27 13:02:39 +0530

    fixlogic idPalindrome

--- basicarrayquestion/IsPalindrome.java
+++ basicarrayquestion/IsPalindrome.java
@@ -27,22 +27,23 @@ public class IsPalindrome {
         int left = 0;
         int right = s.length() - 1;
 
-        while(left < right){
+        while (left < right) {
 
-            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
 
-            while(right < left && !Character.isLetterOrDigit((s.charAt(right)))){
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+            if (Character.toLowerCase(s.charAt(left))
+                    != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            left--;
-            right++;
 
+            left++;
+            right--;
         }
         return true;
     }
